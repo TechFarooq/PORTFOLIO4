c1161ade2c373a9cdbb0e40b605dd00c584f192c
UMAR FAROOQ S <119664720+TechFarooq@users.noreply.github.com>
2025-07-15 12:12:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,6 @@ export default function Home() {
         <TechStackSwitcher />
         <Projects />
         <Timeline />
-        <CodePlayground />
         <Resume />
         <Contact />
         <Footer />
